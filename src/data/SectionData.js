8c1194947e0996a0e28ef123beafdbfe667151f9
wasib <mohammedwasib@gmail.com>
2019-01-08 11:40:35 +0530

    added section data

---
+++ src/data/SectionData.js
@@ -0,0 +1,27 @@
+export const sectionData=[
+    {
+        heading:'Grow your brand using best in class analytics',
+        info:'Improve your social growth using insights on content performance and audience engagement on Facebook and Instagram.',
+        image:'img1.png'
+    },
+    {
+        heading:'',
+        info:'',
+        image:''
+    },
+    {
+        heading:'',
+        info:'',
+        image:''
+    },
+    {
+        heading:'',
+        info:'',
+        image:''
+    },
+    {
+        heading:'',
+        info:'',
+        image:''
+    }
+]
